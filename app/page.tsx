9a48c0a64b5660d300c1f977abbe06857ba7cf5a
asethii <ashinder@gmail.com>
2026-01-27 17:06:21 -0500

    add timeline and update some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,42 @@
 import Image from "next/image";
+import Timeline from "./components/Timeline";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file!!!.
-          </h1>
+        
+        <div className="flex flex-col gap-6 text-center sm:items-start sm:text-left">
+          <div className="flex gap-6 items-start">
+            <Image
+              className="dark:invert flex-shrink-0"
+              src="/headshot.png"
+              alt="Ashish Sethi"
+              width={128}   
+              height={128}
+              priority
+            />
+            <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+              Senior Web Engineer who builds high-impact, user-centered digital experiences.
+            </h1>
+          </div>
           <p>
-            Senior Web Engineer who builds high-impact, user-centered digital experiences.
+            
 For 15+ years, I’ve been designing and engineering modern web solutions across the full stack: React, .NET, C#, ASP.NET, Shopify, Craft CMS, and more. My work blends engineering leadership, UI/UX intuition, accessibility standards, and AI-driven automation to create systems that scale.
 
-I lead projects end-to-end — from concept and prototyping to architecture, development, testing, and launch. I collaborate closely with designers, product, marketing, legal, QA, and executive teams to drive clarity, reduce risk, and deliver outcomes.
 
-Whether I’m rethinking an e-commerce-driven lead funnel, automating partner website generation for thousands of dealers, or building AI tools to improve accessibility and content quality, I focus on one thing:
-
-Delivering fast, intuitive, reliable experiences that make the web better — for users, developers, and businesses.
           </p>
+          <p>I lead projects end-to-end — from concept and prototyping to architecture, development, testing, and launch. I collaborate closely with designers, product, marketing, legal, QA, and executive teams to drive clarity, reduce risk, and deliver outcomes.
+
+
+</p>
+<p>Whether I’m rethinking an e-commerce-driven lead funnel, automating partner website generation for thousands of dealers, or building AI tools to improve accessibility and content quality, I focus on one thing:
+</p>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            Delivering fast, intuitive, reliable experiences that make the web better — for users, developers, and businesses.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        {/* <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -58,7 +50,7 @@ Delivering fast, intuitive, reliable experiences that make the web better — fo
               width={16}
               height={16}
             />
-            Deploy Now
+            Button
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
@@ -66,10 +58,12 @@ Delivering fast, intuitive, reliable experiences that make the web better — fo
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Button
           </a>
-        </div>
+        </div> */}
+        <Timeline />
       </main>
+      
     </div>
   );
 }
